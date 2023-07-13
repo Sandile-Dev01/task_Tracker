f2b5d3aff0213b94653f491517b679c495a3c799
Sandile-Dev01 <sandilegofx@gmail.com>
2023-07-13 10:30:08 +0200

    added buttons and header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,45 @@
 <template>
-  <header>
-    <div>
-      <h1>Hello Vue</h1>
-    </div>
-  </header>
+  <div class="container">
+    <Header />
+  </div>
 </template>
+
+<script>
+import Header from "./components/Header.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Nunito&display=swap");
+
+*,
+html {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+body {
+  font-family: "Nunito", sans-serif;
+}
+
+.container {
+  max-width: 500px;
+  min-height: 300px;
+  border: 2px solid blue;
+  padding: 1.5rem;
+  border-radius: 5px;
+}
+
+.btn {
+  display: inline-block;
+  background-color: red;
+  color: wheat;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header>
+    <h1>Task Tracker</h1>
+    <Button />
+  </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+
+export default {
+  name: "Header",
+  components: { Button },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
